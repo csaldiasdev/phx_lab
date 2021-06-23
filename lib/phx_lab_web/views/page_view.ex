e8c4263710786e3134d9ca7b5859fbe16122339d
csaldiasdev <cristian.saldias.90@outlook.com>
2021-06-22 22:23:34 -0400

    Fixes

--- lib/phx_lab_web/views/page_view.ex
+++ lib/phx_lab_web/views/page_view.ex
@@ -2,7 +2,7 @@ defmodule PhxLabWeb.PageView do
   use PhxLabWeb, :view
 
   def render("pwa.html", _assigns) do
-    Path.join(:code.priv_dir(:phx_lab), "static/build/index.html")
+    Path.join(:code.priv_dir(:phx_lab), "static/index.html")
     |> File.read!()
     |> raw()
   end
