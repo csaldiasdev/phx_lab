0113f1dc2b145cbf84a614a8093a6a6ba499c651
csaldiasdev <cristian.saldias.90@outlook.com>
2021-07-03 23:20:38 -0400

    Cors problems...

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :phx_lab, PhxLabWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "phxlab.gigalixirapp.com", port: 80],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin: ["https://phxlab.gigalixirapp.com", "http://localhost:3000"]
 
 # Do not print debug messages in production
 config :logger, level: :info
